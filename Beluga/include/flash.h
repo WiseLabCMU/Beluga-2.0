7125c2c03f62beb3cef036199f3fda97a533a0cb
Tom Schmitz <tschmitz@andrew.cmu.edu>
2024-06-24 18:13:12 -0400

    Wrote flash module

--- Beluga/include/flash.h
+++ Beluga/include/flash.h
@@ -11,19 +11,9 @@
 
 #ifndef _FLASH_H_
 #define _FLASH_H_
-#include "fds.h"
+
 #include <stdint.h>
 
-#define FILE_ID      0x0015 /* The ID of the file to write the records into. */
-#define RECORD_KEY_1 0x1111 /* A key for the first record. (ID) */
-#define RECORD_KEY_2 0x2222 /* A key for the second record. (BOOTMODE) */
-#define RECORD_KEY_3 0x3333 /* A key for the third record. (RATE)*/
-#define RECORD_KEY_4 0x4444 /* A key for the forth record. (CHANNEL)*/
-#define RECORD_KEY_5 0x5555 /* A key for the fifth record. (BLE Timeout)*/
-#define RECORD_KEY_6 0x6666 /* A key for the sixth record. (TX Power)*/
-#define RECORD_KEY_7 0x7777 /* A key for the seventh record. (STREAMMODE)*/
-#define RECORD_KEY_8 0x8888 /* A key for the eighth record. (TWRMODE)*/
-#define RECORD_KEY_9 0x9999 /* A key for the ninth record. (LEDMODE)*/
 #define CONFIG_ID    1      /* Idenfication for ID configuration */
 #define CONFIG_BM    2      /* Idenfication for Boot mode configuration */
 #define CONFIG_RATE  3      /* Idenfication for polling rate configuration */
@@ -35,8 +25,9 @@
     8 /* Idenfication for two way raning scheme mode configuration */
 #define CONFIG_LED 9 /* Idenfication for LED mode configuration */
 
-void fds_evt_handler(fds_evt_t const *p_fds_evt);
-void writeFlashID(uint32_t id, int record);
-uint32_t getFlashID(uint32_t record_key);
+int32_t initFlash(void);
+void writeFlashID(uint32_t id, int32_t record);
+int32_t readFlashID(uint32_t id);
+void eraseRecords(void);
 
 #endif
--- Beluga/src/flash.c
+++ Beluga/src/flash.c
@@ -9,162 +9,102 @@
  *  @author WiseLab-CMU
  */
 
-#include "flash.h"
-#include "fds.h"
+#include <flash.h>
+#include <zephyr/kernel.h>
+#include <zephyr/drivers/flash.h>
+#include <zephyr/storage/flash_map.h>
+#include <zephyr/device.h>
+#include <zephyr/devicetree.h>
 #include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 
-/**@brief FDS event handler to handle errors during initialization */
-void fds_evt_handler(fds_evt_t const *p_fds_evt) {
-    switch (p_fds_evt->id) {
-    case FDS_EVT_INIT:
-        if (p_fds_evt->result != FDS_SUCCESS) {
-            // Initialization failed.
-        }
-        break;
-    default:
-        break;
+#define FILE_ID      0x0015 /* The ID of the file to write the records into. */
+#define RECORD_KEY_1 0 /* A key for the first record. (ID) */
+#define RECORD_KEY_2 1 /* A key for the second record. (BOOTMODE) */
+#define RECORD_KEY_3 2 /* A key for the third record. (RATE)*/
+#define RECORD_KEY_4 3 /* A key for the forth record. (CHANNEL)*/
+#define RECORD_KEY_5 4 /* A key for the fifth record. (BLE Timeout)*/
+#define RECORD_KEY_6 5 /* A key for the sixth record. (TX Power)*/
+#define RECORD_KEY_7 6 /* A key for the seventh record. (STREAMMODE)*/
+#define RECORD_KEY_8 7 /* A key for the eighth record. (TWRMODE)*/
+#define RECORD_KEY_9 8 /* A key for the ninth record. (LEDMODE)*/
+
+#define PARTITION storage_partition
+
+#define PARTITION_OFFSET FIXED_PARTITION_OFFSET(PARTITION)
+#define PARTITION_DEVICE FIXED_PARTITION_DEVICE(PARTITION)
+
+#define FLASH_PAGE_SIZE 4096
+
+static const struct device *flash_dev = PARTITION_DEVICE;
+
+static uint32_t id2key(uint32_t record) {
+    uint32_t record_key = 0;
+    switch(record) {
+        case CONFIG_ID:
+            record_key = RECORD_KEY_1;
+            break;
+        case CONFIG_BM:
+            record_key = RECORD_KEY_2;
+            break;
+        case CONFIG_RATE:
+            record_key = RECORD_KEY_3;
+            break;
+        case CONFIG_CHAN:
+            record_key = RECORD_KEY_4;
+            break;
+        case CONFIG_TIME:
+            record_key = RECORD_KEY_5;
+            break;
+        case CONFIG_TXP:
+            record_key = RECORD_KEY_6;
+            break;
+        case CONFIG_SM:
+            record_key = RECORD_KEY_7;
+            break;
+        case CONFIG_TWR:
+            record_key = RECORD_KEY_8;
+            break;
+        case CONFIG_LED:
+            record_key = RECORD_KEY_9;
+            break;
+        default:
+            assert_print("Invalid record");
+    }
+    return record_key;
+}
+
+int32_t initFlash(void) {
+    if (!device_is_ready(flash_dev)) {
+        printk("Flash device is not ready\n");
+        return 1;
+    }
+    return 0;
+}
+
+void writeFlashID(uint32_t id, int32_t record) {
+    uint32_t record_key = id2key(id);
+    off_t offset = PARTITION_OFFSET + (record_key << 2);
+
+    if (flash_write(flash_dev, offset, &record, sizeof(int32_t)) != 0) {
+        printk("Flash write failed!\n");
     }
 }
 
-/**
- * @brief Modify storage information
- */
-void writeFlashID(uint32_t id, int record) {
+int32_t readFlashID(uint32_t id) {
+    uint32_t record_key = id2key(id);
+    off_t offset = PARTITION_OFFSET + (record_key << 2);
+    int32_t value;
 
-    uint32_t record_key;
-    if (record == CONFIG_ID)
-        record_key = RECORD_KEY_1;
-    if (record == CONFIG_BM)
-        record_key = RECORD_KEY_2;
-    if (record == CONFIG_RATE)
-        record_key = RECORD_KEY_3;
-    if (record == CONFIG_CHAN)
-        record_key = RECORD_KEY_4;
-    if (record == CONFIG_TIME)
-        record_key = RECORD_KEY_5;
-    if (record == CONFIG_TXP)
-        record_key = RECORD_KEY_6;
-    if (record == CONFIG_SM)
-        record_key = RECORD_KEY_7;
-    if (record == CONFIG_TWR)
-        record_key = RECORD_KEY_8;
-    if (record == CONFIG_LED)
-        record_key = RECORD_KEY_9;
-
-    static char str[(sizeof("1") + 3) / 4];
-    sprintf(str, "%d", id);
-
-    fds_record_desc_t record_desc_2;
-    fds_find_token_t ftok_2;
-
-    /* It is required to zero the token before first use. */
-    memset(&ftok_2, 0x00, sizeof(fds_find_token_t));
-
-    // Write a new record if the record not exist in flash
-    if (fds_record_find(FILE_ID, record_key, &record_desc_2, &ftok_2) !=
-        FDS_SUCCESS) {
-
-        fds_record_t record;
-        fds_record_desc_t record_desc;
-
-        // Set up record.
-        record.file_id = FILE_ID;
-        record.key = record_key;
-        record.data.p_data = &str;
-        record.data.length_words =
-            (sizeof("1") + 3) / 4; /* one word is four bytes. */
-
-        ret_code_t rc;
-        rc = fds_record_write(&record_desc, &record);
-        if (rc != FDS_SUCCESS) {
-            printf("Write 1 Failed\r\n"); /* Handle error. */
-        }
+    if (flash_read(flash_dev, offset, &value, sizeof(int32_t)) != 0) {
+        printk("Flash read failed!\n");
+        return 0;
     }
 
-    fds_flash_record_t flash_record;
-    fds_record_desc_t record_desc_1;
-    fds_find_token_t ftok;
-    /* It is required to zero the token before first use. */
-    memset(&ftok, 0x00, sizeof(fds_find_token_t));
-
-    /* Loop until all records with the given key and file ID have been found. */
-    /* Update a new record if the record exist in flash */
-    while (fds_record_find(FILE_ID, record_key, &record_desc_1, &ftok) ==
-           FDS_SUCCESS) {
-
-        if (fds_record_open(&record_desc_1, &flash_record) != FDS_SUCCESS) {
-            printf("error opening\r\n"); /* Handle error. */
-        }
-
-        if (fds_record_close(&record_desc_1) != FDS_SUCCESS) {
-            printf("close error\r\n"); /* Handle error. */
-        }
-
-        fds_record_t record;
-
-        // Set up record.
-        record.file_id = FILE_ID;
-        record.key = record_key;
-        record.data.p_data = &str;
-        record.data.length_words =
-            (sizeof(str) + 3) / 4; /* one word is four bytes. */
-        ret_code_t rc;
-        rc = fds_record_update(&record_desc_1, &record);
-        if (rc != FDS_SUCCESS) {
-            printf("UPDATE ERROR\r\n"); /* Handle error. */
-        }
-    }
+    return value;
 }
 
-/**
- * @brief Retrive storage information
- */
-uint32_t getFlashID(uint32_t record_key) {
-
-    uint32_t rec;
-    if (record_key == CONFIG_ID)
-        rec = RECORD_KEY_1;
-    else if (record_key == CONFIG_BM)
-        rec = RECORD_KEY_2;
-    else if (record_key == CONFIG_RATE)
-        rec = RECORD_KEY_3;
-    else if (record_key == CONFIG_CHAN)
-        rec = RECORD_KEY_4;
-    else if (record_key == CONFIG_TIME)
-        rec = RECORD_KEY_5;
-    else if (record_key == CONFIG_TXP)
-        rec = RECORD_KEY_6;
-    else if (record_key == CONFIG_SM)
-        rec = RECORD_KEY_7;
-    else if (record_key == CONFIG_TWR)
-        rec = RECORD_KEY_8;
-    else if (record_key == CONFIG_LED)
-        rec = RECORD_KEY_9;
-
-    uint32_t ret_val;
-    fds_flash_record_t flash_record;
-    fds_record_desc_t record_desc_1;
-    fds_find_token_t ftok;
-
-    /* It is required to zero the token before first use. */
-    memset(&ftok, 0x00, sizeof(fds_find_token_t));
-
-    while (fds_record_find(FILE_ID, rec, &record_desc_1, &ftok) ==
-           FDS_SUCCESS) {
-
-        if (fds_record_open(&record_desc_1, &flash_record) != FDS_SUCCESS) {
-            printf("error opening\r\n"); /* Handle error. */
-        }
-
-        /* Access the record through the flash_record structure. */
-        ret_val = atoi(flash_record.p_data);
-
-        if (fds_record_close(&record_desc_1) != FDS_SUCCESS) {
-            printf("close error\r\n"); /* Handle error. */
-        }
+void eraseRecords(void) {
+    if (flash_erase(flash_dev, PARTITION_OFFSET, FLASH_PAGE_SIZE) != 0) {
+        printk("flash erase failed!\n");
     }
-
-    return ret_val;
 }
