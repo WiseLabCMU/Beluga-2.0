309f2a33111f45ef08b03c6f8f462f12ee8673db
Tom Schmitz <tschmitz@andrew.cmu.edu>
2024-07-11 19:23:18 -0400

    Fix off by one error

--- Beluga/src/uart.c
+++ Beluga/src/uart.c
@@ -33,7 +33,7 @@ static void serial_callback(const struct device *dev, void *user_data) {
     }
 
     while (uart_fifo_read(uart, &c, 1) == 1) {
-        if ((c == '\n' || c == '\r') && rx_buf.len > 0) {
+        if ((c == '\n' || c == '\r' || c == '\0') && rx_buf.len > 0) {
             struct buffer *_buf;
             rx_buf.data[rx_buf.len] = '\0';
 
@@ -42,14 +42,14 @@ static void serial_callback(const struct device *dev, void *user_data) {
                 printk("Unable to allocate fifo item\n");
                 break;
             }
-            _buf->len = rx_buf.len;
+            _buf->len = rx_buf.len + 1;
             _buf->buf = k_malloc(_buf->len);
             if (_buf->buf == NULL) {
                 printk("Unable to allocate buffer for fifo item\n");
                 k_free(_buf);
                 return;
             }
-            memcpy(_buf->buf, rx_buf.data, rx_buf.len);
+            memcpy(_buf->buf, rx_buf.data, rx_buf.len + 1);
             k_fifo_put(&uart_rx_queue, _buf);
             rx_buf.len = 0;
             printk("Placed item into fifo\n");
