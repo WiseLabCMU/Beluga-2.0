2238952421a21cf53963e513a586100c3d728341
Tom Schmitz <tschmitz@andrew.cmu.edu>
2024-07-31 15:52:52 -0400

    Fix bug in driver port

--- Beluga/DecaDriver/src/port_platform.c
+++ Beluga/DecaDriver/src/port_platform.c
@@ -162,11 +162,14 @@ int writetospi(uint16 headerLength, const uint8 *headerBuffer,
  * not be driven high externally.
  * */
 void reset_DW1000(void) {
+    #ifndef CONFIG_ENABLE_BELUGA_UWB
+    return;
+    #endif
     gpio_pin_configure_dt(&dw1000_reset_pin, GPIO_OUTPUT);
     gpio_pin_set_dt(&dw1000_reset_pin, 0);
     k_msleep(2);
-    gpio_pin_set_dt(&dw1000_reset_pin, 1);
-    k_msleep(50);
+    // gpio_pin_set_dt(&dw1000_reset_pin, 1);
+    // k_msleep(50);
     gpio_pin_configure_dt(&dw1000_reset_pin, GPIO_DISCONNECTED);
     k_msleep(2);
 }
