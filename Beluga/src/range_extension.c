de4636c87ac9e7ce9317aefaa377d0ef4c3176cf
Tom Schmitz <tschmitz@andrew.cmu.edu>
2024-08-06 11:27:30 -0400

    Add power table

--- Beluga/src/range_extension.c
+++ Beluga/src/range_extension.c
@@ -9,6 +9,62 @@
 #ifdef CONFIG_FEM_AL_LIB
 #include <fem_al/fem_al.h>
 #include <deca_device_api.h>
+#include <nrf.h>
+
+const uint32_t nrf_power_value[] = {
+#if defined(RADIO_TXPOWER_TXPOWER_Neg40dBm)
+        RADIO_TXPOWER_TXPOWER_Neg40dBm,
+#endif /* RADIO_TXPOWER_TXPOWER_Neg40dBm */
+        RADIO_TXPOWER_TXPOWER_Neg30dBm,
+        RADIO_TXPOWER_TXPOWER_Neg20dBm,
+        RADIO_TXPOWER_TXPOWER_Neg16dBm,
+        RADIO_TXPOWER_TXPOWER_Neg12dBm,
+        RADIO_TXPOWER_TXPOWER_Neg8dBm,
+#if defined(RADIO_TXPOWER_TXPOWER_Neg7dBm)
+        RADIO_TXPOWER_TXPOWER_Neg7dBm,
+#endif /* defined(RADIO_TXPOWER_TXPOWER_Neg7dBm) */
+#if defined(RADIO_TXPOWER_TXPOWER_Neg6dBm)
+        RADIO_TXPOWER_TXPOWER_Neg6dBm,
+#endif /* defined(RADIO_TXPOWER_TXPOWER_Neg6dBm) */
+#if defined(RADIO_TXPOWER_TXPOWER_Neg5dBm)
+        RADIO_TXPOWER_TXPOWER_Neg5dBm,
+#endif /* defined(RADIO_TXPOWER_TXPOWER_Neg5dBm) */
+        RADIO_TXPOWER_TXPOWER_Neg4dBm,
+#if defined(RADIO_TXPOWER_TXPOWER_Neg3dBm)
+        RADIO_TXPOWER_TXPOWER_Neg3dBm,
+#endif /* defined (RADIO_TXPOWER_TXPOWER_Neg3dBm) */
+#if defined(RADIO_TXPOWER_TXPOWER_Neg2dBm)
+        RADIO_TXPOWER_TXPOWER_Neg2dBm,
+#endif /* defined (RADIO_TXPOWER_TXPOWER_Neg2dBm) */
+#if defined(RADIO_TXPOWER_TXPOWER_Neg1dBm)
+        RADIO_TXPOWER_TXPOWER_Neg1dBm,
+#endif /* defined (RADIO_TXPOWER_TXPOWER_Neg1dBm) */
+        RADIO_TXPOWER_TXPOWER_0dBm,
+#if defined(RADIO_TXPOWER_TXPOWER_Pos1dBm)
+        RADIO_TXPOWER_TXPOWER_Pos1dBm,
+#endif /* RADIO_TXPOWER_TXPOWER_Pos1dBm */
+#if defined(RADIO_TXPOWER_TXPOWER_Pos2dBm)
+        RADIO_TXPOWER_TXPOWER_Pos2dBm,
+#endif /* defined(RADIO_TXPOWER_TXPOWER_Pos2dBm) */
+#if defined(RADIO_TXPOWER_TXPOWER_Pos3dBm)
+        RADIO_TXPOWER_TXPOWER_Pos3dBm,
+#endif /* defined(RADIO_TXPOWER_TXPOWER_Pos3dBm) */
+#if defined(RADIO_TXPOWER_TXPOWER_Pos4dBm)
+        RADIO_TXPOWER_TXPOWER_Pos4dBm,
+#endif /* defined(RADIO_TXPOWER_TXPOWER_Pos4dBm) */
+#if defined(RADIO_TXPOWER_TXPOWER_Pos5dBm)
+        RADIO_TXPOWER_TXPOWER_Pos5dBm,
+#endif /* defined(RADIO_TXPOWER_TXPOWER_Pos5dBm) */
+#if defined(RADIO_TXPOWER_TXPOWER_Pos6dBm)
+        RADIO_TXPOWER_TXPOWER_Pos6dBm,
+#endif /* defined(RADIO_TXPOWER_TXPOWER_Pos6dBm) */
+#if defined(RADIO_TXPOWER_TXPOWER_Pos7dBm)
+        RADIO_TXPOWER_TXPOWER_Pos7dBm,
+#endif /* defined(RADIO_TXPOWER_TXPOWER_Pos7dBm) */
+#if defined(RADIO_TXPOWER_TXPOWER_Pos8dBm)
+        RADIO_TXPOWER_TXPOWER_Pos8dBm
+#endif /* defined(RADIO_TXPOWER_TXPOWER_Pos8dBm) */
+};
 
 // TODO
 
