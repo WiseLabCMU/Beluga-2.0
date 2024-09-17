e1f04b982df9b8f3bde45661574881c365c1d781
Tom Schmitz <tschmitz@andrew.cmu.edu>
2024-09-17 16:08:40 -0400

    Add option to debug BLE

--- Beluga/src/ble_app.c
+++ Beluga/src/ble_app.c
@@ -172,6 +172,9 @@ static void insert_into_seen_list(struct ble_data *data, int8_t rssi) {
     }
     seen_list[last_seen_index].UUID = data->uuid;
     seen_list[last_seen_index].RSSI = rssi;
+    if (IS_ENABLED(CONFIG_BELUGA_EVAL_BLE_STRENGTH)) {
+        seen_list[index].update_flag = 1;
+    }
     seen_list[last_seen_index].ble_time_stamp = k_uptime_get();
     if (data->manufacturerData[POLLING_FLAG_INDEX] == '0') {
         seen_list[last_seen_index].polling_flag = 0;
@@ -186,6 +189,9 @@ static void insert_into_seen_list(struct ble_data *data, int8_t rssi) {
 static void update_seen_neighbor(struct ble_data *data, int8_t rssi) {
     int32_t index = get_seen_list_index(data->uuid);
     seen_list[index].RSSI = rssi;
+    if (IS_ENABLED(CONFIG_BELUGA_EVAL_BLE_STRENGTH)) {
+        seen_list[index].update_flag = 1;
+    }
     seen_list[index].ble_time_stamp = k_uptime_get();
 
     if (data->manufacturerData[POLLING_FLAG_INDEX] == '0') {
