7f418cabdff919443ff0f040f1f7ef8bd18262a0
Tom Schmitz <tschmitz@andrew.cmu.edu>
2024-12-03 17:28:40 -0500

    Redefined beluga hardware

--- boards/arm/beluga2/board.cmake
+++
@@ -1,9 +0,0 @@
-# Copyright (c) 2024 Nordic Semiconductor ASA
-# SPDX-License-Identifier: Apache-2.0
-
-board_runner_args(jlink "--device=nrf52832_xxaa" "--speed=4000" "--reset-after-load")
-board_runner_args(pyocd "--target=nrf52832")
-set(OPENOCD_NRF5_SUBFAMILY "nrf52")
-include(${ZEPHYR_BASE}/boards/common/jlink.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/pyocd.board.cmake)
-include(${ZEPHYR_BASE}/boards/common/openocd-nrf5.board.cmake)
