e84fa3995bad7b31732773d05b1c2a3a6f45a6de
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-01-07 16:37:43 -0500

    Documented serial LEDs

--- Beluga/include/serial_led.h
+++ Beluga/include/serial_led.h
@@ -1,16 +1,59 @@
-//
-// Created by tom on 12/10/24.
-//
+/**
+ * @file serial_led.c
+ * @brief Serial LED Control for Communication Status Indication
+ *
+ * Implements the logic for controlling serial LEDs that indicate the
+ * state of serial communication. It provides functions for configuring,
+ * initializing, and updating the LED states based on transmission (TX) and
+ * reception (RX) events.
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ * @date 12/10/24
+ */
 
 #ifndef BELUGA_SERIAL_LED_H
 #define BELUGA_SERIAL_LED_H
 
 #include <stdbool.h>
 
-enum serial_led_state { LED_START_TX, LED_STOP_TX, LED_START_RX, LED_STOP_RX };
+/**
+ * Defines the possible states for the serial LEDs, representing
+ * the transmission and reception states of the serial communication.
+ * The state of the LEDs can be updated based on whether the system is
+ * transmitting (TX) or receiving (RX) data.
+ */
+enum serial_led_state {
+    LED_START_TX, ///< Serial transmission started
+    LED_STOP_TX,  ///< Serial transmission ended
+    LED_START_RX, ///< Serial reception started
+    LED_STOP_RX   ///< Serial reception ended
+};
 
+/**
+ * @brief Initializes the serial LEDs
+ * @return 0 upon success
+ * @return negative error code otherwise
+ */
 int serial_leds_init(void);
+
+/**
+ * @brief Updates the LED states based on the current serial communication state
+ *
+ * @param[in] state The serial communication state
+ *
+ * @return 0 upon success
+ * @return -EINVAL for an invalid state
+ * @return negative error code otherwise
+ */
 int serial_leds_update_state(enum serial_led_state state);
+
+/**
+ * @brief Enables or disables the serial LEDs
+ * @param[in] enable `true` if the serial LEDs are to be enabled, `false` if the
+ * serial LEDs are to be disabled.
+ * @return 0 upon success
+ * @return -ETIMEDOUT if unable to disable LED state updates within 10ms
+ */
 int set_serial_led_master_state(bool enable);
 
 #endif // BELUGA_SERIAL_LED_H
--- Beluga/src/serial_led.c
+++ Beluga/src/serial_led.c
@@ -1,38 +1,97 @@
-//
-// Created by tom on 12/10/24.
-//
+/**
+ * @file serial_led.c
+ * @brief Serial LED Control for Communication Status Indication
+ *
+ * Implements the logic for controlling serial LEDs that indicate the
+ * state of serial communication. It provides functions for configuring,
+ * initializing, and updating the LED states based on transmission (TX) and
+ * reception (RX) events.
+ *
+ * @author Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ * @date 12/10/24
+ */
 
 #include <serial_led.h>
 #include <zephyr/device.h>
+
+/**
+ * The node defining the LEDs being used for serial communication
+ */
+#define SERIAL_LEDS_NODE DT_PATH(serial_leds)
+
+#if defined(CONFIG_SERIAL_LEDS) && DT_NODE_EXISTS(SERIAL_LEDS_NODE)
 #include <zephyr/drivers/gpio.h>
 #include <zephyr/kernel.h>
 #include <zephyr/logging/log.h>
 
-#define SERIAL_LEDS_NODE DT_PATH(serial_leds)
-
-#if IS_ENABLED(CONFIG_SERIAL_LEDS) && DT_NODE_EXISTS(SERIAL_LEDS_NODE)
-
+/**
+ * Logger for the serial LEDs
+ */
 LOG_MODULE_REGISTER(serial_led, CONFIG_SERIAL_LED_LOG_LEVEL);
 
-#define GPIO0_DEV                DEVICE_DT_GET(DT_NODELABEL(gpio0))
-#define GPIO1_DEV                DEVICE_DT_GET_OR_NULL(DT_NODELABEL(gpio1))
+/**
+ * GPIO bank 0 device
+ */
+#define GPIO0_DEV DEVICE_DT_GET(DT_NODELABEL(gpio0))
 
+/**
+ * GPIO bank 1 device
+ */
+#define GPIO1_DEV DEVICE_DT_GET_OR_NULL(DT_NODELABEL(gpio1))
+
+/**
+ * Helper macro for getting gpio devicetree specs and inserting commas in
+ * between
+ */
 #define GPIO_SPEC_AND_COMMA(led) GPIO_DT_SPEC_GET(led, gpios),
 
+/**
+ * The GPIO specs for the serial LEDs
+ */
 static const struct gpio_dt_spec leds[] = {
 #if DT_NODE_EXISTS(SERIAL_LEDS_NODE)
     DT_FOREACH_CHILD(SERIAL_LEDS_NODE, GPIO_SPEC_AND_COMMA)
 #endif
 };
 
+/**
+ * Bit mask for no LEDs
+ */
 #define LEDS_NONE_MASK 0x0
-#define LEDS_ALL_MASK  (((uint32_t)1 << ARRAY_SIZE(leds)) - 1)
 
-#define LED_TX_MASK    BIT(0)
-#define LED_RX_MASK    (LEDS_ALL_MASK & BIT(1))
+/**
+ * Bit mask for all the LEDs
+ */
+#define LEDS_ALL_MASK (((uint32_t)1 << ARRAY_SIZE(leds)) - 1)
 
+/**
+ * The bit mask for the TX LED
+ */
+#define LED_TX_MASK BIT(0)
+
+/**
+ * The bit mask for the RX LED
+ */
+#define LED_RX_MASK (LEDS_ALL_MASK & BIT(1))
+
+/**
+ * Flag indicating whether to enable/disable the serial LEDs
+ */
 static atomic_t serial_led_on = ATOMIC_INIT(1);
 
+/**
+ * @brief Updates the states of the serial LEDs to on or off
+ *
+ * @param[in] leds_on_mask The LEDs to turn on
+ * @param[in] leds_off_mask The LEDs to turn off
+ *
+ * @note If an LED is not specified in either mask, that LED's
+ * state is not updated
+ *
+ * @return 0 upon success
+ * @return -EINVAL if either mask is invalid
+ * @return negative error code otherwise
+ */
 static int serial_leds_set_state(uint32_t leds_on_mask,
                                  uint32_t leds_off_mask) {
     if ((leds_on_mask & ~LEDS_ALL_MASK) != 0 ||
@@ -61,6 +120,11 @@ static int serial_leds_set_state(uint32_t leds_on_mask,
     return 0;
 }
 
+/**
+ * @brief Initializes the serial LEDs
+ * @return 0 upon success
+ * @return negative error code otherwise
+ */
 int serial_leds_init(void) {
     int err;
 
@@ -75,8 +139,21 @@ int serial_leds_init(void) {
     return serial_leds_set_state(LEDS_NONE_MASK, LEDS_ALL_MASK);
 }
 
+/**
+ * Reference counter indicating how many threads are actively updating thread
+ * states
+ */
 atomic_t ref_count = ATOMIC_INIT(0);
 
+/**
+ * @brief Updates the LED states based on the current serial communication state
+ *
+ * @param[in] state The serial communication state
+ *
+ * @return 0 upon success
+ * @return -EINVAL for an invalid state
+ * @return negative error code otherwise
+ */
 int serial_leds_update_state(enum serial_led_state state) {
     int err;
 
@@ -109,6 +186,13 @@ int serial_leds_update_state(enum serial_led_state state) {
     return err;
 }
 
+/**
+ * @brief Enables or disables the serial LEDs
+ * @param[in] enable `true` if the serial LEDs are to be enabled, `false` if the
+ * serial LEDs are to be disabled.
+ * @return 0 upon success
+ * @return -ETIMEDOUT if unable to disable LED state updates within 10ms
+ */
 int set_serial_led_master_state(bool enable) {
     int ret = 0;
 
@@ -136,15 +220,37 @@ int set_serial_led_master_state(bool enable) {
 }
 
 #else
+
+/**
+ * @brief Initializes the serial LEDs
+ * @return 0 upon success
+ * @return negative error code otherwise
+ */
 int serial_leds_init(void) { return 0; }
 
+/**
+ * @brief Updates the LED states based on the current serial communication state
+ *
+ * @param[in] state The serial communication state
+ *
+ * @return 0 upon success
+ * @return -EINVAL for an invalid state
+ * @return negative error code otherwise
+ */
 int serial_leds_update_state(enum serial_led_state state) {
     ARG_UNUSED(state);
     return 0;
 }
 
+/**
+ * @brief Enables or disables the serial LEDs
+ * @param[in] enable `true` if the serial LEDs are to be enabled, `false` if the
+ * serial LEDs are to be disabled.
+ * @return 0 upon success
+ * @return -ETIMEDOUT if unable to disable LED state updates within 10ms
+ */
 int set_serial_led_master_state(bool enable) {
     ARG_UNUSED(enable);
     return 0;
 }
-#endif // IS_ENABLED(CONFIG_SERIAL_LEDS) &&  DT_NODE_EXISTS(SERIAL_LEDS_NODE)
+#endif // defined(CONFIG_SERIAL_LEDS) &&  DT_NODE_EXISTS(SERIAL_LEDS_NODE)
