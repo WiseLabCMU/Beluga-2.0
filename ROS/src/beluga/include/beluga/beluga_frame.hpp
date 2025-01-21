47f0ca95dda9f7cf3bec17b2e6dc381eff589356
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-01-20 21:16:09 -0500

    Translate beluga frame to C++

---
+++ ROS/src/beluga/include/beluga/beluga_frame.hpp
@@ -0,0 +1,81 @@
+/**
+ * @file beluga_frame.h
+ *
+ * @brief
+ *
+ * @date 1/20/25
+ *
+ * @author tom
+ */
+
+#ifndef BELUGA_BELUGA_FRAME_H
+#define BELUGA_BELUGA_FRAME_H
+
+#include <string>
+#include <utility>
+#include <vector>
+#include <variant>
+#include <exception>
+
+namespace Beluga {
+
+    class BelugaFrameError : public std::exception {
+    public:
+        explicit BelugaFrameError(std::string error) : message(std::move(error)) {}
+
+        [[nodiscard]] const char *what() const noexcept override {
+            return message.c_str();
+        }
+
+    private:
+        std::string message;
+    };
+
+    class BelugaFrame {
+    public:
+        BelugaFrame();
+        explicit BelugaFrame(const std::string &str);
+        explicit BelugaFrame(const char *str, size_t len);
+        ~BelugaFrame();
+
+        enum BelugaFrameType {
+            COMMAND_RESPONSE,
+            NEIGHBOR_UPDATE,
+            RANGING_EVENT,
+            NEIGHBOR_DROP,
+            START_EVENT,
+            NO_TYPE
+        };
+
+        struct NeighborUpdate {
+            uint16_t ID;
+            int8_t RSSI;
+            double RANGE;
+            int64_t TIMESTAMP;
+            uint32_t EXCHANGE;
+        };
+
+        struct RangeEvent {
+            uint16_t ID;
+            uint32_t EXCHANGE;
+        };
+
+        struct DecodedFrame {
+            BelugaFrame::BelugaFrameType type;
+            std::variant<std::string, std::vector<BelugaFrame::NeighborUpdate>, BelugaFrame::RangeEvent, uint32_t> payload;
+        };
+
+        static std::tuple<ssize_t, ssize_t, ssize_t> frame_present(const char *bytearray, size_t len, bool error_no_footer = true);
+        static std::tuple<ssize_t, ssize_t, ssize_t> frame_present(const std::string &bytearray, bool error_no_footer = true);
+
+        void parse_frame(const char *serial_data, size_t start_index, size_t len);
+        void parse_frame(const std::string &serial_data, size_t start_index);
+
+        [[nodiscard]] BelugaFrame::DecodedFrame get_parsed_data() const;
+
+    private:
+        BelugaFrame::DecodedFrame parsed_data;
+    };
+};
+
+#endif //BELUGA_BELUGA_FRAME_H
---
+++ ROS/src/beluga/src/beluga_frame.cpp
@@ -0,0 +1,156 @@
+/**
+ * @file beluga_frame.cpp
+ *
+ * @brief
+ *
+ * @date 1/20/25
+ *
+ * @author tom
+ */
+
+#include <beluga/beluga_frame.hpp>
+#include <daw/json/daw_json_link.h>
+
+#define BELUGA_MSG_HEADER          (uint8_t)'@'
+#define BELUGA_MSG_FOOTER          (uint8_t)'*'
+
+#define BELUGA_MSG_HEADER_OVERHEAD 1
+#define BELUGA_MSG_LEN_OVERHEAD    2
+#define BELUGA_MSG_TYPE_OVERHEAD   1
+#define BELUGA_MSG_FOOTER_OVERHEAD 1
+
+#define BELUGA_HEADER_OVERHEAD (BELUGA_MSG_HEADER_OVERHEAD + BELUGA_MSG_LEN_OVERHEAD + BELUGA_MSG_TYPE_OVERHEAD)
+#define BELUGA_FRAME_OVERHEAD (BELUGA_HEADER_OVERHEAD + BELUGA_MSG_FOOTER_OVERHEAD)
+
+#define MSG_HEADER_OFFSET  0
+#define MSG_LEN_OFFSET     (MSG_HEADER_OFFSET + BELUGA_MSG_HEADER_OVERHEAD)
+#define MSG_TYPE_OFFSET    (MSG_LEN_OFFSET + BELUGA_MSG_LEN_OVERHEAD)
+#define MSG_PAYLOAD_OFFSET (MSG_TYPE_OFFSET + BELUGA_MSG_TYPE_OVERHEAD)
+
+#define CONSTRUCT_PAYLOAD_LEN(array_, i) ((uint16_t)(uint8_t)(array_)[i] | (uint16_t)((uint8_t)(array_)[i + 1] << __CHAR_BIT__))
+
+namespace daw::json {
+    template<>
+    struct json_data_contract<Beluga::BelugaFrame::NeighborUpdate> {
+        using type = json_member_list<json_number<"ID", uint16_t>, json_number<"RSSI", int8_t>, json_number<"RANGE", double>, json_number<"TIMESTAMP", int64_t>, json_number<"EXCHANGE", uint32_t>>;
+    };
+
+    template<>
+    struct json_data_contract<Beluga::BelugaFrame::RangeEvent> {
+        using type = json_member_list<json_number<"ID", uint16_t>, json_number<"EXCHANGE", uint32_t>>;
+    };
+}
+
+Beluga::BelugaFrame::BelugaFrame() {
+    parsed_data.type = NO_TYPE;
+}
+
+Beluga::BelugaFrame::BelugaFrame(const std::string& str) {
+    auto [start_index, length, bytes_left] = Beluga::BelugaFrame::frame_present(str);
+
+    if (start_index >= 0) {
+        parse_frame(str, start_index);
+    } else {
+        throw BelugaFrameError("Frame is not present");
+    }
+}
+
+Beluga::BelugaFrame::BelugaFrame(const char *str, size_t len) {
+    auto [start_index, length, bytes_left] = Beluga::BelugaFrame::frame_present(std::string(str, len));
+
+    if (start_index >= 0) {
+        parse_frame(std::string(str, len), start_index);
+    } else {
+        throw BelugaFrameError("Frame is not present");
+    }
+}
+
+void Beluga::BelugaFrame::parse_frame(const char *serial_data, size_t start_index, size_t len) {
+    this->parse_frame(std::string(serial_data, len), start_index);
+}
+#include <iostream>
+void Beluga::BelugaFrame::parse_frame(const std::string &serial_data, size_t start_index) {
+    size_t payload_len = CONSTRUCT_PAYLOAD_LEN(serial_data, start_index + MSG_LEN_OFFSET);
+    BelugaFrameType type = (BelugaFrameType)serial_data[start_index + MSG_TYPE_OFFSET];
+    std::string _payload = serial_data.substr(start_index + MSG_PAYLOAD_OFFSET, payload_len);
+    size_t last_index;
+
+    try {
+        switch (type) {
+            case COMMAND_RESPONSE:
+            case START_EVENT:
+                this->parsed_data.payload = _payload;
+                break;
+            case NEIGHBOR_UPDATE:
+                this->parsed_data.payload = daw::json::from_json_array<NeighborUpdate>(_payload);
+                break;
+            case RANGING_EVENT:
+                this->parsed_data.payload = daw::json::from_json<RangeEvent>(_payload);
+                break;
+            case NEIGHBOR_DROP:
+                this->parsed_data.payload = (uint32_t) stoull(_payload, &last_index);
+                if (last_index != payload_len) {
+                    throw std::invalid_argument("");
+                }
+                break;
+            default:
+                type = NO_TYPE;
+                break;
+        }
+    } catch (daw::json::json_exception const &jex) {
+        throw BelugaFrameError(jex.reason());
+    } catch (std::invalid_argument const &invalid) {
+        throw BelugaFrameError("Unable to convert integer due to it not being an integer");
+    } catch (std::out_of_range const &range) {
+        throw BelugaFrameError("Integer passed in is out of range");
+    }
+
+    this->parsed_data.type = type;
+}
+
+Beluga::BelugaFrame::DecodedFrame Beluga::BelugaFrame::get_parsed_data() const {
+    return this->parsed_data;
+}
+
+std::tuple<ssize_t, ssize_t, ssize_t> Beluga::BelugaFrame::frame_present(const char *bytearray, size_t len, bool error_no_footer) {
+    return frame_present(std::string(bytearray, len), error_no_footer);
+}
+
+std::tuple<ssize_t, ssize_t, ssize_t>
+Beluga::BelugaFrame::frame_present(const std::string &bytearray, bool error_no_footer) {
+    size_t len = bytearray.size();
+    ssize_t header_index, type_index, payload_len_index, frame_size;
+    size_t payload_len, footer_index;
+
+    for (ssize_t i = 0; i < len; i++) {
+        if ((uint8_t)bytearray[i] != BELUGA_MSG_HEADER) {
+            continue;
+        }
+        header_index = i;
+
+        type_index = header_index + MSG_TYPE_OFFSET;
+        if (type_index > len) {
+            continue;
+        }
+
+        payload_len_index = header_index + MSG_LEN_OFFSET;
+        payload_len = CONSTRUCT_PAYLOAD_LEN(bytearray, payload_len_index);
+
+        footer_index = header_index + BELUGA_HEADER_OVERHEAD + payload_len;
+
+        if (footer_index >= len && error_no_footer) {
+            continue;
+        }
+
+        if (error_no_footer && (uint8_t)bytearray[footer_index] != BELUGA_MSG_FOOTER) {
+            continue;
+        }
+
+        frame_size = BELUGA_FRAME_OVERHEAD + payload_len;
+        return {header_index, frame_size, footer_index - (len - header_index) + 1};
+    }
+
+    return {-1, -1, -1};
+}
+
+Beluga::BelugaFrame::~BelugaFrame() = default;
