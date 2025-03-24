18975c5c74c9d414aaed05cdf08c1e5eafc9570f
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-03-24 12:06:04 -0400

    Document Beluga queue

--- serial-comms/cpp/beluga-serial/include/beluga/beluga_queue.hpp
+++ serial-comms/cpp/beluga-serial/include/beluga/beluga_queue.hpp
@@ -1,3 +1,12 @@
+/**
+ * @file beluga_queue.hpp
+ *
+ * @brief Thread safe implementation of a queue
+ *
+ * @date 1/30/25
+ *
+ * @authro Tom Schmitz \<tschmitz@andrew.cmu.edu\>
+ */
 #ifndef _BELUGA_QUEUE
 #define _BELUGA_QUEUE
 
@@ -10,9 +19,20 @@
 #include <string>
 
 namespace BelugaSerial {
+
+/// Exception class for BelugaQueue
 class BelugaQueueException : public std::exception {
   public:
+    /**
+     * @brief Enum for queue exception reasons
+     */
     enum QueueExceptionReason { QUEUE_EMPTY, QUEUE_FULL, QUEUE_TIMEOUT };
+
+    /**
+     * @brief Constructor for BelugaQueueException
+     *
+     * @param[in] reason The reason for the exception
+     */
     explicit BelugaQueueException(const QueueExceptionReason &reason)
         : reason_(reason) {
         switch (reason) {
@@ -31,10 +51,18 @@ class BelugaQueueException : public std::exception {
         }
     }
 
+    /**
+     * The reason for the exception as a string
+     * @return The error reason
+     */
     [[nodiscard]] const char *what() const noexcept override {
         return what_.c_str();
     }
 
+    /**
+     * @brief Get the reason for the exception
+     * @return The reason for the exception
+     */
     [[nodiscard]] QueueExceptionReason reason() const noexcept {
         return reason_;
     }
@@ -44,18 +72,62 @@ class BelugaQueueException : public std::exception {
     std::string what_;
 };
 
+/// Thread safe implementation of a queue. The template parameters indicate the
+/// type of the queued messages, the max size of the queue, and whether to
+/// overwrite old messages when the queue is full. This queue implementation
+/// does not use dynamic memory, but instead uses a ring buffer, making it more
+/// safe since the needed memory is known at compile time.
 template <typename Type, size_t max_size = 1, bool overwrite = false>
 class BelugaQueue {
   public:
+    /**
+     * @brief Put an item into the queue
+     *
+     * @param[in] item The item to put into the queue
+     * @param[in] block If `true`, block until space is available, otherwise
+     * throw an exception or overwrite the oldest item depending on the
+     * configuration
+     * @param[in] timeout_ms The timeout in milliseconds to wait for space to
+     * become available
+     * @throws BelugaQueueException If the queue is full
+     */
     void put(Type &item, bool block = true,
              const std::chrono::milliseconds &timeout_ms =
                  std::chrono::milliseconds::zero());
+
+    /**
+     * @brief Get an item from the queue
+     * @param[in] block If `true`, block until an item is available, otherwise
+     * throw an exception
+     * @param[in] timeout_ms The timeout in milliseconds to wait for an item to
+     * become available
+     * @return The oldest item in the queue
+     * @throws BelugaQueueException If the queue is empty
+     */
     Type get(bool block = true, const std::chrono::milliseconds &timeout_ms =
                                     std::chrono::milliseconds::zero());
 
+    /**
+     * The number of items in the queue
+     * @return The number of items in the queue
+     */
     size_t size();
+
+    /**
+     * Check if the queue is empty
+     * @return `true` if the queue is empty, `false` otherwise
+     */
     bool empty();
+
+    /**
+     * Check if the queue is full
+     * @return `true` if the queue is full, `false` otherwise
+     */
     bool full();
+
+    /**
+     * Clear the queue
+     */
     void clear();
 
   private:
