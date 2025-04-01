89139467a978f611d0e894db7dc51a36dd69e58b
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-04-01 19:19:19 -0400

    Add UWB puch button, reboot button and apply power button

--- interface_app/beluga_gui/widgets/beluga_pushbuttons.py
+++ interface_app/beluga_gui/widgets/beluga_pushbuttons.py
@@ -4,6 +4,9 @@ from .beluga_widget import BelugaWidgetBase
 from typing import Optional, Callable
 
 
+DEBUG = True
+
+
 class BelugaPushButton(QPushButton):
     def __init__(self, parent: Optional[QWidget]):
         super().__init__(parent)
@@ -26,6 +29,8 @@ class BleButton(BelugaPushButton, BelugaWidgetBase):
         super().__init__(parent)
         self._ble_update = self.BleUpdateEmitter()
         self.ble_update = {"bool": self._ble_update.bool_update}
+        if DEBUG:
+            self._handler = self.test_handler
 
     def set_ble_state(self, running: bool):
         self._ble_running = running
@@ -38,3 +43,43 @@ class BleButton(BelugaPushButton, BelugaWidgetBase):
     def update(self):
         enable = not self._uwb_running
         self.setEnabled(enable)
+
+    def test_handler(self):
+        state = not self._ble_running
+        self.set_ble_state(state)
+
+
+class UwbButton(BelugaPushButton, BelugaWidgetBase):
+    class UwbUpdateEmitter(QObject):
+        bool_update = pyqtSignal(bool)
+
+    def __init__(self, parent: Optional[QWidget]):
+        super().__init__(parent)
+        self._uwb_update = self.UwbUpdateEmitter()
+        self.uwb_update = {"bool": self._uwb_update.bool_update}
+        if DEBUG:
+            self._handler = self.test_handler
+
+    def set_uwb_state(self, running: bool):
+        self._uwb_running = running
+        if self._uwb_running:
+            self.setText("Stop UWB")
+        else:
+            self.setText("Start UWB")
+        self._uwb_update.bool_update.emit(self._uwb_running)
+
+    def update(self):
+        enable = self._ble_running
+        self.setEnabled(enable)
+
+    def test_handler(self):
+        state = not self._uwb_running
+        self.set_uwb_state(state)
+
+
+class RebootButton(BelugaPushButton):
+    pass
+
+class ApplyPowerButton(BelugaPushButton):
+    def update_checkbox_state(self, state: bool):
+        self.setEnabled(not state)
