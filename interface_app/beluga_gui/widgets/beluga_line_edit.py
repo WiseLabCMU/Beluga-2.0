c8f41fa6efd5682882e53c9134416f9cc49b729b
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-04-01 20:13:04 -0400

    Create Beluga line edits

---
+++ interface_app/beluga_gui/widgets/beluga_line_edit.py
@@ -0,0 +1,40 @@
+from PyQt5.QtWidgets import QLineEdit, QWidget
+from .beluga_widget import BelugaWidgetBase
+from typing import Optional, Callable
+
+
+class BelugaLineEdit(QLineEdit):
+    def __init__(self, parent: Optional[QWidget]):
+        super().__init__(parent)
+        self._handler: Optional[Callable[[str], None]] = None
+        self.returnPressed.connect(self.handle_return_pressed)
+
+    def set_handler(self, handler: Optional[Callable[[str], None]] = None):
+        self._handler = handler
+
+    def handle_return_pressed(self):
+        print(self.text())
+        if self._handler is not None:
+            self._handler(self.text())
+
+
+class UwbLineEdit(BelugaLineEdit, BelugaWidgetBase):
+    def update(self):
+        self.setEnabled(not self._uwb_running)
+        self._buddy_sig.bool_update.emit(not self._uwb_running)
+
+
+class NodeIdLineEdit(UwbLineEdit):
+    pass
+
+
+class PollRateLineEdit(BelugaLineEdit):
+    pass
+
+
+class TimeoutLineEdit(BelugaLineEdit):
+    pass
+
+
+class UwbPanIdLineEdit(UwbLineEdit):
+    pass
