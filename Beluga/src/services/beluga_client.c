7680068a4db0462b99d8f9b610136ae14c1b7bc9
Tom Schmitz <tschmitz@andrew.cmu.edu>
2025-05-01 17:13:10 -0400

    Write function to discover and assign handles

--- Beluga/src/services/beluga_client.c
+++ Beluga/src/services/beluga_client.c
@@ -120,3 +120,67 @@ int bt_beluga_client_sync(struct bt_beluga_client *client,
 
     return err;
 }
+
+int bt_beluga_client_assign(struct bt_gatt_dm *dm,
+                            struct bt_beluga_client *client) {
+    const struct bt_gatt_dm_attr *gatt_service_attr;
+    const struct bt_gatt_service_val *gatt_service;
+    const struct bt_gatt_dm_attr *gatt_chrc;
+    const struct bt_gatt_dm_attr *gatt_desc;
+
+    if (!dm || !client) {
+        return -EFAULT;
+    }
+
+    gatt_service_attr = bt_gatt_dm_service_get(dm);
+    gatt_service = bt_gatt_dm_attr_service_val(gatt_service_attr);
+
+    if (bt_uuid_cmp(gatt_service->uuid, BT_UUID_BELUGA_SVC)) {
+        return -ENOTSUP;
+    }
+    LOG_DBG("Getting handles from Beluga service");
+    memset(&client->handles, 0xFF, sizeof(client->handles));
+
+    /* Range characteristic */
+    gatt_chrc = bt_gatt_dm_char_by_uuid(dm, BT_UUID_BELUGA_RANGE);
+    if (!gatt_chrc) {
+        LOG_ERR("Missing ranging characteristic");
+        return -EINVAL;
+    }
+
+    /* Ranging */
+    gatt_desc = bt_gatt_dm_desc_by_uuid(dm, gatt_chrc, BT_UUID_BELUGA_RANGE);
+    if (!gatt_desc) {
+        LOG_ERR("Missing Beluga Ranging value descriptor in characteristic.");
+        return -EINVAL;
+    }
+    LOG_DBG("Found handle for ranging characteristic.");
+    client->handles.range = gatt_desc->handle;
+    /* Ranging CCC */
+    gatt_desc = bt_gatt_dm_desc_by_uuid(dm, gatt_chrc, BT_UUID_GATT_CCC);
+    if (!gatt_desc) {
+        LOG_ERR("Missing ranging CCC in characteristics");
+        return -EINVAL;
+    }
+    LOG_DBG("Found handle for CCC of ranging characteristic");
+    client->handles.range_ccc = gatt_desc->handle;
+
+    /* Sync characteristic */
+    gatt_chrc = bt_gatt_dm_char_by_uuid(dm, BT_UUID_BELUGA_SYNC);
+    if (!gatt_chrc) {
+        LOG_ERR("Missing sync characteristic");
+        return -EINVAL;
+    }
+    /* Sync */
+    gatt_desc = bt_gatt_dm_desc_by_uuid(dm, gatt_chrc, BT_UUID_BELUGA_SYNC);
+    if (!gatt_desc) {
+        LOG_ERR("Missing sync value descriptor in characteristic");
+        return -EINVAL;
+    }
+    LOG_DBG("Found handle for sync characteristic");
+    client->handles.sync = gatt_desc->handle;
+
+    /* Assign connection instance */
+    client->conn = bt_gatt_dm_conn_get(dm);
+    return 0;
+}
